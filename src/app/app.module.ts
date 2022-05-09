92a035b48e5e9627885e9fe2cc6a9c26d0d927a1
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2022-05-09 10:43:10 +0100

    add library and secondary entry point

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Lib1Module } from 'lib1';
+import { FeatureModule } from 'lib1/feature';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, Lib1Module, FeatureModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
